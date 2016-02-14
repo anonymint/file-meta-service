bfb58309b981064e6e76ab60be200f52f16f63be
Anonymint <mint.com@gmail.com>
2016-02-14 13:17:31 +1100

    Update both front and backend to use multer package

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,8 +1,8 @@
 'use stricts';
-
 var path = process.cwd();
-var Controller = require(path + '/app/controller/fileController.js');
-var timeController = new Controller();
+var multer  = require('multer');
+var storage = multer.memoryStorage();
+var upload = multer({ storage: storage, limits: { fileSize: 5000000 } }).single('0');
 
 module.exports = function(app) {
 
@@ -11,12 +11,23 @@ module.exports = function(app) {
 			res.sendFile(path + "/public/index.html");
 		});
 
+	app.route('/upload')
+		.get(function(req,res) {
+			res.sendFile(path + "/public/upload.html");
+		});
+
 	app.route('/api/upload')
 		.get(function(req, res){
 			res.send('Upload API doesn\'t support get');
 		})
 		.post(function(req,res) {
-
+			upload(req, res, function(err){
+				if (err) {
+					res.send({data: "something wrong with file"});
+				} else {
+					res.send({fileSize: req.file.size, fileName: req.file.originalname});
+				}								
+			});
 		});
 
 	app.route('/*')
